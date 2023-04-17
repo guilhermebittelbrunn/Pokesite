a1b445dfdb7424858c59ce80d293f9a1b27411fc
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2023-04-17 00:02:35 -0300

    add inicial function
    fetch get all some dados of pokemons as name, id e type

--- client/src/App.js
+++ client/src/App.js
@@ -4,28 +4,58 @@ import axios from 'axios';
 
 function App(props) {
     const [data, setData] = useState([]);
+    const [text, setText] = useState();
+
+    async function getAllPokemons(interval) {
+        for (let i = 1; i <= interval; i++) {
+            const apiData = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`);
+            const pokeData = await apiData.data;
+            setData((preventValue) => {
+                return [...preventValue, pokeData];
+            });
+        }
+    }
+
+    async function getOnePokemon(index) {
+        let apiData = await axios.get(`https://pokeapi.co/api/v2/pokemon/${index}`);
+        let pokeData = await apiData.data;
+        setData([pokeData]);
+    }
+
+    async function searchPokemon(e) {
+        (async () => {
+            await setText(e.target.value);
+            console.log(text);
+        })();
+
+        // let tamanho = String(inputValue).length;
+        // setData(
+        //     data.filter((poke) => {
+        //         let pokenome = String(poke.name).slice(0, tamanho);
+        //         // console.log(pokenome, '-', text);
+        //         if (pokenome === inputValue) {
+        //             console.log(poke);
+        //             return poke;
+        //         }
+        //     })
+        // );
+    }
+
+    function showText() {
+        console.log(text);
+    }
 
     useEffect(() => {
-        for (let i = 1; i <= 30; i++) {
-            findData(i);
-        }
+        getAllPokemons(50);
+        // getOnePokemon('c');
+        console.log(data);
     }, []);
-
-    async function findData(i) {
-        await axios.get('https://pokeapi.co/api/v2/pokemon/' + i).then((res) => {
-            setData((preventValue) => {
-                return [...preventValue, res.data];
-            });
-        });
-    }
-
-    for (let poke of data) {
-        console.log(poke.name, poke.id);
-    }
 
     return (
         <>
             <h4>Site</h4>
+            <input type="text" onChange={searchPokemon}></input>
+            <button onClick={showText}>{'Click'}</button>
             <Card data={data} />
         </>
     );
